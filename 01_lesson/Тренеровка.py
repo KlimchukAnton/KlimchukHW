f462bfe5c8b6a13a716f62330f07738b3a126150
Anton <9s1m7@e-record.com>
2025-07-14 23:33:08 +1000

    HomeWork

---
+++ 01_lesson/Тренеровка.py
@@ -0,0 +1,23 @@
+my_heigh=176
+print(my_heigh)
+
+my_name= "Anton"
+my_name= "Anton Klimchuk"
+print(my_name)
+
+pet_name= input("Как зовут вашего питомца?")
+print("Ваш любимчик- "+pet_name)
+
+def print_python():
+    print("Учу Python!")
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+print_letter('С')
+print_letter('Т')
+print_letter('У')
+print_letter('Д')
+print_letter('Е')
+print_letter('Н')
+print_letter('Т')
